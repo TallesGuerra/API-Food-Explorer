aec2bdd7243582b702c59f56444d4b6545de7e25
talles <talles-guerra@hotmail.com>
2025-01-03 14:42:26 +0000

    routes created » users.routes and index.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const app = express();
+const routes = require("./routes");
 
-app.get("/", (request, response) => {
-   response.send("Hello, World!") 
-});
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
