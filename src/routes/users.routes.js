f6a793e6b24625df177c34b627e5fb0739bf4d16
talles <talles-guerra@hotmail.com>
2025-01-13 14:22:21 +0000

    hash created, database updated and Knex installed and config

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,21 +4,18 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
+/*
 function myMiddleware(request, response, next) {
-    console.log("Você passou pelo Middleware.");
-    
-    if(!request.body.isAdmin){
+     if (!request.body.isAdmin) {
         return response.json({ message: "user unauthorized" });
     }
-
-
     next();
 };
+*/
 
 
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
-
+usersRoutes.post("/", /* myMiddleware */ usersController.create);
+usersRoutes.put("/:id", usersController.update)
 module.exports = usersRoutes;
