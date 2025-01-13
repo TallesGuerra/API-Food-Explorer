ef5feef6136ac09266e1b91a0f03cc06d6a09c2d
talles <talles-guerra@hotmail.com>
2025-01-13 16:49:34 +0000

    Tags Migrations created, Dishes migrations updated

--- src/database/knex/migrations/20250113162657_createDishes.js
+++ src/database/knex/migrations/20250113162657_createDishes.js
@@ -1,13 +1,16 @@
-exports.up = knex => knex.schema.createTable("dish", table => {
+exports.up = knex => knex.schema.createTable("dishes", table => {
     table.increments("id")
     table.text("title")    
+    table.varchar("image")    
     table.text("description")
     table.text("ingredients")    
     table.integer("price")
+
+    table.integer("user_id").references("id").inTable("users")
 
     
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
 });
 
-exports.down =  knex => knex.schema.dropTable("dish");
+exports.down =  knex => knex.schema.dropTable("dishes");
